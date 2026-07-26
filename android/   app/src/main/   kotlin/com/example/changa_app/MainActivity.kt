f98667fefc61a574fad76f8b36da140f7c66d4cd
israbarreto110-sys <israbarreto110@gmail.com>
2026-07-26 20:08:29 -0300

    Update MainActivity.kt

--- android/   app/src/main/   kotlin/com/example/changa_app/MainActivity.kt
+++ android/   app/src/main/   kotlin/com/example/changa_app/MainActivity.kt
@@ -1,6 +1,6 @@
-   package com.example.changa_app
+package com.example.changa_app
 
-   import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-   class MainActivity: FlutterActivity() {
-   }
+class MainActivity: FlutterFragmentActivity() {
+}
